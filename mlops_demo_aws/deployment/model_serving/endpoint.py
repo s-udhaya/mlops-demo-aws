465e46b3490af21cb6de12d63dab1cbbf1cff3c7
Udhayaraj Sivalingam <udhayaraj12@gmail.com>
2023-06-04 20:20:29 +0200

    update endpoint to preview

--- mlops_demo_aws/deployment/model_serving/endpoint.py
+++ mlops_demo_aws/deployment/model_serving/endpoint.py
@@ -2,8 +2,8 @@ from enum import Enum
 
 
 class Endpoint(Enum):
-    SERVING = "api/2.0/serving-endpoints"
+    SERVING = "api/2.0/preview/serving-endpoints"
     INVOCATIONS = "realtime-inference/{}/invocations"
-    SERVED_MODELS = "api/2.0/serving-endpoints/{}/served-models"
-    EVENTS = "api/2.0/serving-endpoints/{}/events"
-    CONFIG = "api/2.0/serving-endpoints/{}/config"
+    SERVED_MODELS = "api/2.0/preview/serving-endpoints/{}/served-models"
+    EVENTS = "api/2.0/preview/serving-endpoints/{}/events"
+    CONFIG = "api/2.0/preview/serving-endpoints/{}/config"
