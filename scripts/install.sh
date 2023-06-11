ac1b173f281257f1a50d065d9621f154e0b9751e
Udhayaraj Sivalingam <udhayaraj12@gmail.com>
2023-06-11 12:58:31 +0200

    add databricks cli

---
+++ scripts/install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+# Make sure the target directory is writable.
+if [ ! -w "$TARGET" ]; then
+    echo "Target directory $TARGET is not writable."
+    echo "Please run this script through sudo to allow writing to $TARGET."
+    exit 1
+fi
+
+# Make sure we don't overwrite an existing installation.
+if [ -f "$TARGET/databricks" ]; then
+    echo "Target path $TARGET/databricks already exists."
+    exit 1
+fi
+
+# Change into temporary directory.
+cd "$(mktemp -d)"
+
+# Download release archive.
+
+# Unzip release archive.
+unzip -q "scripts/databricks_cli_linux_amd64.zip"
+
+# Add databricks to path.
+chmod +x ./databricks
+cp ./databricks "$TARGET"
+echo "Installed $($TARGET/databricks -v) at $TARGET/databricks."
