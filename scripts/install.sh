551799f325e832f81f344df98ac3eeef57d204d6
Udhayaraj Sivalingam <udhayaraj12@gmail.com>
2023-06-11 13:19:16 +0200

    add databricks cli

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,15 +15,18 @@ if [ -f "$TARGET/databricks" ]; then
     exit 1
 fi
 
+echo "creating temp directory"
 # Change into temporary directory.
 cd "$(mktemp -d)"
 
 # Download release archive.
 
+echo "unzipping databricks cli"
 # Unzip release archive.
 unzip -q -o "scripts/databricks_cli_linux_amd64.zip"
 
+echo "adding databricks to path"
 # Add databricks to path.
-chmod +x ./databricks
+sudo chmod +x ./databricks
 sudo cp ./databricks "$TARGET"
 echo "Installed $($TARGET/databricks -v) at $TARGET/databricks."
